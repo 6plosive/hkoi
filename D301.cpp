066e1c0c8b28f93813f7c77238c14cc9c632180e
6plosive <101194078+6plosive@users.noreply.github.com>
2022-11-26 03:24:49 +0000

    GOOFYAHHH

---
+++ D301.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void D301(){
+	string nis[5];
+	for(string& pe:nis) getline(cin,pe);
+	cout<<"HELLO!!! I AM "<<nis[0]<<" AND I LOVE "<<nis[1]<<". I F**KING HATE "<<nis[2]<<" AND "<<nis[3]<<" LOVER GRRR I F**KING HATE THEM!!!!"<<endl;
+	cout<<"lets go "<<nis[4]<<" and get some bitches\n";
+}
+
+int main(){
+	D301();
+}
