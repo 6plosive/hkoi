fceea5af9a97e37b8c807e327d758df24e5f3b4a
6plosive <101194078+6plosive@users.noreply.github.com>
2022-11-26 12:32:16 +0000

    asda

--- M17A9.cpp
+++ M17A9.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(){
 	//string a;
 	//getline(cin,a);
-	//this_thread::sleep_for(chrono::milliseconds(8964));
+	this_thread::sleep_for(chrono::milliseconds(8964));
 	cout<<"I go to school by bus."<<endl;
 }
