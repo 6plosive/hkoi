55618b8b8a36e19fcce286c80b8e325a4780fe18
6plosive <101194078+6plosive@users.noreply.github.com>
2022-11-26 12:32:02 +0000

    haha funny NOT FUNNY AT ALL OMG

---
+++ M17A9.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	//string a;
+	//getline(cin,a);
+	//this_thread::sleep_for(chrono::milliseconds(8964));
+	cout<<"I go to school by bus."<<endl;
+}
