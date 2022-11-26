f742692f91022da55b3cd2f40876721b584db54a
6plosive <101194078+6plosive@users.noreply.github.com>
2022-11-26 08:57:41 +0000

    uahsdiuahs

---
+++ D103.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void D103(){
+	int a,b,c;
+	cin>>a>>b>>c;
+	cout<<fixed<<setprecision(3)<<(a*b*sin(c*M_PI/180))/2<<endl;
+}
+
+int main(){
+	D103();
+}
