86b8eefde15d15380abd3f32b0e536bd7b9046f4
6plosive <101194078+6plosive@users.noreply.github.com>
2022-12-01 01:02:04 +0000

    I DONT FUKCINGN NWOWKNIWOWOW

--- M1022.cpp
+++ M1022.cpp
@@ -16,6 +16,8 @@ bool lol(vector<pair<int,int>>& list,int __i,int ymod){
 }
 
 int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 	int n,x,y;
 	cin>>n>>x>>y;
 	vector<pair<int,int>> list(n);
@@ -28,16 +30,28 @@ int main(){
 		if(lol(list,i,y)){
 			find.push_back(make_pair(list[i].first+x,list[i].second));
 		}
-		if(!find.empty()) while(list[i].first>find.front().first) find.erase(find.begin());
-		if(find.front().first==list[i].first){
-			for(int j=i;j<list.size();j++){
-				if(list[i].first==list[j].first){
-					if(list[i].second+y==list[j].second){
-						count++;
-						find.erase(find.begin());
-						break;
-					}else if(list[i].second+y<list[j].second) break;
-				}else break;
+		//cout<<list[i].first<<' '<<list[i].second;
+		//if(!find.empty()) while(list[i]>find.front()) find.erase(find.begin());
+		//if(!find.empty()) while(list[i].second>find.front().second) find.erase(find.begin());
+		if(!find.empty()) while((list[i].first>=find.front().first&&list[i].second>find.front().second)){
+			if(find.empty()) break;
+			find.erase(find.begin());
+		}
+		//cout<<"now:"<<list[i].first<<' '<<list[i].second<<endl;
+		//for(auto l:find) cout<<l.first<<' '<<l.second<<endl;
+		//cout<<endl<<endl;
+		if(!find.empty()){
+			if(find.front()==list[i]){
+				//cout<<find.front().first<<' '<<find.front().second<<endl;
+				for(int j=i;j<list.size();j++){
+					if(list[i].first==list[j].first){
+						if(list[i].second+y==list[j].second){
+							count++;
+							find.erase(find.begin());
+							break;
+						}else if(list[i].second+y<list[j].second) break;
+					}else break;
+				}
 			}
 		}
 	}
