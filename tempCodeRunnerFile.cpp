b93b9b4f779ff7aa00e1995fbb6f53a2e52c18e1
6plosive <101194078+6plosive@users.noreply.github.com>
2022-12-01 01:29:31 +0000

    dfg

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,44 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool lol(vector<pair<int,int>>& list,int __i,int ymod){
-	for(int i=__i;i<list.size();i++){
-		if(list[__i].first==list[i].first){
-			if(list[__i].second+ymod==list[i].second){
-				return true;
-			}
-			if(list[__i].second+ymod<list[i].second) return false;
-		}else{//already useless cuz not same x
-			return false;
-		}
-	}
-	return false;
-}
-
 int main(){
-	int n,x,y;
-	cin>>n>>x>>y;
-	vector<pair<int,int>> list(n);
-	vector<pair<int,int>> find;
-	for(auto& l:list) cin>>l.first>>l.second;
-	sort(list.begin(),list.end());
+	string target,search;
+	cin>>search>>target;
 	int count=0;
-	//for(auto l:list) cout<<l[0]<<l[1]<<endl;
-	for(int i=0;i<list.size();i++){
-		if(lol(list,i,y)){
-			find.push_back(make_pair(list[i].first+x,list[i].second));
-		}
-		if(find.front()==list[i]){
-			for(int j=i;j<list.size();j++){
-				if(list[i].first==list[j].first){
-					if(list[i].second+y==list[j].second){
-						count++;
-						find.erase(find.begin());
-						break;
-					}else if(list[i].second+y>list[j].second) break;
-				}else break;
-			}
-		}
+	bool nice=true;
+	int iterate = search.length()-target.length()+1;
+	for(int i=0;i<iterate;i++){
+		if(search.substr(i,target.length())==target)count++;
 	}
 	cout<<count<<endl;
 }
